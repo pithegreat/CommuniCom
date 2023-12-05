5cbbd816890b2990dbe6658defb3396272faa574
cyrusmase <cyrusmase@gmail.com>
2023-12-05 13:19:16 -0800

    nav added
    buttons added as well as removal of a home button as it is not necessary. this can be fixed by adding a button at the bottom of the page of information. allowing return to the home page.

--- app/src/main/java/com/example/communicom/FE_HS_Home.java
+++ app/src/main/java/com/example/communicom/FE_HS_Home.java
@@ -13,11 +13,6 @@ import android.widget.Toast;
 
 public class FE_HS_Home extends AppCompatActivity {
 
-    Spinner pageSelectionSpinner;
-    String pageSelectionString, confirmDataString;
-    TextView currentPage_Header;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,27 +21,6 @@ public class FE_HS_Home extends AppCompatActivity {
         CharSequence text = "Sending message...";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(this /* MyActivity */, text, duration);
-
-        Intent confirmIntent = getIntent();
-        Bundle confirmBundle = confirmIntent.getExtras();
-
-        confirmDataString = confirmBundle.getString("selectionString");
-        pageSelectionSpinner = findViewById(R.id.pageSelSpinner);
-
-        //location selection spinner
-        //location selection adapter
-
-        ArrayAdapter<CharSequence> adapter =
-                ArrayAdapter.createFromResource(this,
-                        R.array.pageSelArray, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        pageSelectionSpinner.setAdapter(adapter);
-        currentPage_Header = findViewById(R.id.currentPage_Header);
-        currentPage_Header.setText(pageSelectionSpinner.getSelectedItem().toString());
-
-        //output with  string = locationSelection.getSelectedItem().toString();
-
-
 
     }
 }
