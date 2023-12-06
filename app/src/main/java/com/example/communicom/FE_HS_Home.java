b771f0f2d321941de6c15ae7b5c020dbede1ea04
cyrusmase <cyrusmase@gmail.com>
2023-12-06 11:56:54 -0800

    MASS_REVERT
    removal of nav

--- app/src/main/java/com/example/communicom/FE_HS_Home.java
+++ app/src/main/java/com/example/communicom/FE_HS_Home.java
@@ -1,10 +1,7 @@
 package com.example.communicom;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,46 +12,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
 public class FE_HS_Home extends AppCompatActivity
 {
     String versionN_H, versionF_H, versionR_H, versionM_H;
-    TabLayout navigation;
-    ViewPager2 viewPager;
-    ViewPagerAdapter viewPagerAdapter;
 
-    @SuppressLint("MissingInflatedId")
-    protected void onCreate(Bundle savedInstanceState)
-    {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fe_hs_home);
-
-        viewPager = findViewById(R.id.vp);
-        navigation = findViewById(R.id.Navs);
-
-        ViewPagerAdapter adapt = new ViewPagerAdapter(getSupportFragmentManager(),getLifecycle());
-        viewPager.setAdapter(adapt);
-
-        new TabLayoutMediator(navigation,viewPager,(tab, position) -> {
-            switch (position){
-                case 0: tab.setText("Home");
-                break;
-                case 1: tab.setText("News");
-                    break;
-                case 2: tab.setText("Forums");
-                    break;
-                case 3: tab.setText("Resources");
-                    break;
-                case 4: tab.setText("Map");
-                    break;
-            }
-        }).attach();
-    }
-}
- /*   @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -62,7 +24,7 @@ public class FE_HS_Home extends AppCompatActivity
 
         CharSequence text = "Sending message...";
         int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(this /* MyActivity *//*, text, duration);
+        Toast toast = Toast.makeText(this /* MyActivity */, text, duration);
     }
     public void onNewsClickH(View view){
         Intent intentN = new Intent(this, FE_HS_News.class);
@@ -96,4 +58,4 @@ public class FE_HS_Home extends AppCompatActivity
 
         finish();
     }
-}*/
+}
