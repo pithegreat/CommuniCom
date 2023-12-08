aba39dc1b62546c6d02d520a50fcc58e9b714722
cyrusmase <cyrusmase@gmail.com>
2023-12-07 19:58:38 -0800

    submit news output to string FIXED
    working commit

--- app/src/main/java/com/example/communicom/FE_Form_SubmitNews.java
+++ app/src/main/java/com/example/communicom/FE_Form_SubmitNews.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,28 +21,45 @@ import java.io.OutputStreamWriter;
 public class FE_Form_SubmitNews extends AppCompatActivity {
 
     private static final String FILE_NAME = "form_submitNews";
+    public CheckBox susView, missingView, comm_aView, hazardView, weatherView, otherChkView;
+    public EditText otherEntryView, commentsView;
+    public Spinner locationSelectionView;
+    public String sus, missing, comm_a, hazard, weather, other, location, time, comments;
+    CharSequence text;
+    int duration;
+    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fe_form_submit_news);
 
+        text = "Sending message...";
+        duration = Toast.LENGTH_SHORT;
+        toast = Toast.makeText(this /* MyActivity */, text, duration);
+
+        susView = findViewById(R.id.susCheckbox);
+        missingView = findViewById(R.id.missingCheckbox);
+        comm_aView = findViewById(R.id.comm_aCheckbox);
+        hazardView = findViewById(R.id.hazardCheckbox);
+        weatherView = findViewById(R.id.weatherCheckbox);
+        otherChkView = findViewById(R.id.checkBoxOther);
+        otherEntryView = findViewById(R.id.otherEntry);
+        commentsView = findViewById(R.id.additionalText);
 
 
-        String[] elements = {};
-        writeToFile(this, FILE_NAME, elements);
 
-        /*studentNumber = w_studentNumEdit.getText().toString();
-        lastName = w_lastEdit.getText().toString();
-        firstName = w_firstEdit.getText().toString();
-        gender = w_genderRGroup.getText().toString();
-        division = w_divSpinner.getSelectedItem().toString();
-        fullName = lastName + " , " + firstName;
-        i++;
-        saveCounterToPreferences(i);
-        String[] elements = {String.valueOf(i), studentNumber, fullName, gender, division};
-        writeToFile(this, FILE_NAME, elements);*/
+        locationSelectionView = findViewById(R.id.form_submitNews_locationSelection);
+        //location selection adapter
+        ArrayAdapter<CharSequence> locationAdapter =
+                ArrayAdapter.createFromResource(this,
+                        R.array.locationsArray, android.R.layout.simple_spinner_item);
+        locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        locationSelectionView.setAdapter(locationAdapter);
+        //output with  string = locationSelection.getSelectedItem().toString();
+        location = locationSelectionView.getSelectedItem().toString();
     }
+
     public void isOtherChecked(View view){
         CheckBox checkBoxOther = findViewById(R.id.checkBoxOther);
         TextView otherTextView = findViewById(R.id.otherTextView);
@@ -52,22 +74,29 @@ public class FE_Form_SubmitNews extends AppCompatActivity {
             otherEntry.setVisibility(View.INVISIBLE);
         }
     }
-    public static void writeToFile(Context context, String fileName, String[] elements) {
-        try {
-            // Open the file in append mode, if the file doesn't exist, it will be created
-            FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_APPEND);
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+    public void onRadioButtonPressed(View view){
 
-            // Iterate through the elements and append them to the file
-            for (String element : elements) {
-                outputStreamWriter.write(element + ", ");//
-            }
-            outputStreamWriter.write("\n");
-            // Close the streams
-            outputStreamWriter.close();
-            fileOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        RadioGroup radioGroup = findViewById(R.id.timeRadioGroup);
+        int selectedID = radioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton = findViewById(selectedID);
+        time = radioButton.getText().toString();
+    }
+    public void onSubmitNewsButtonPressed(View view){
+        if(susView.isChecked()){sus = "Suspicious Activity";}
+        if(missingView.isChecked()){missing = "Missing Items";}
+        if(comm_aView.isChecked()){comm_a = "Community Alert";}
+        if(hazardView.isChecked()){hazard = "Potential Hazards";}
+        if(weatherView.isChecked()){weather = "Weather Conditions";}
+        if(otherChkView.isChecked()){other = otherEntryView.getText().toString();}
+
+        comments = commentsView.getText().toString();
+
+        location = locationSelectionView.getSelectedItem().toString();
+        //all outputs working
+        String newsElementsTrial = ("sus: "+sus+" lo:  "+location+" ti: "+time);
+        toast.setText(newsElementsTrial);
+        //String[] newsElements = {sus,missing,comm_a,hazard,weather,other};
+        toast.show();
+        //writeToFile(this, FILE_NAME, newsElements);
     }
 }
